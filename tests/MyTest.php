c603923a25484b9d71eb0a777144fcdbc52abe29
Rodrigo Pires Meira <rpmeir@gmail.com>
2021-01-12 01:26:32 -0300

    setup: initial setup

---
+++ tests/MyTest.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class MyTest extends TestCase
+{
+    public function testMyTest(): void
+    {
+        self::assertTrue(false);
+    }
+}
